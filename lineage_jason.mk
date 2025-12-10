7cb6ec2f5d187ba741d20b75c1e42ae57058cea7
EndCredits <me@leemina.moe>
2025-12-10 22:54:37 +0800

    jason: do not override the build fingerprint
    Signed-off-by: EndCredits <me@leemina.moe>

--- lineage_jason.mk
+++ lineage_jason.mk
@@ -38,9 +38,6 @@ PRODUCT_CHARACTERISTICS := nosdcard,tablet
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="jason" \
-    PRODUCT_NAME="jason" \
-    PRIVATE_BUILD_DESC="jason-user 9 PKQ1.181007.001 V12.0.1.0.PCHCNXM release-keys"
-
-BUILD_FINGERPRINT := "Xiaomi/jason/jason:9/PKQ1.181007.001/V12.0.1.0.PCHCNXM:user/release-keys"
+    PRODUCT_NAME="jason"
 
 TARGET_VENDOR := Xiaomi
